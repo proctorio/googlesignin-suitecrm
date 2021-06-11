47c7e81e9a50ed1c3b317b71037aacaa471b74f9
Rohit Menon <rohit@proctorio.com>
2021-06-11 17:50:21 +0000

    fix bad script defer

--- SugarModules/modules/Users/Login.php
+++ SugarModules/modules/Users/Login.php
@@ -234,8 +234,7 @@ if (
 
 $google_signin_clientid = $sugar_config['google_signin_clientid'];
 
-echo $AAA = <<<EOQ
-	<script src="https://apis.google.com/js/platform.js" async defer></script>
+echo $AAA = <<<EOQ	
 	<style>
 	#user_name, #username_password, #bigbutton { display:none; }
 	</style>
@@ -246,6 +245,7 @@ echo $AAA = <<<EOQ
 			if(googleSignInClientId != '') {
 				$('head').append('<meta name="google-signin-client_id" content="'+googleSignInClientId+'" />');
 				$('form').find('input[type="submit"]').after('<div>OR</div><div class="g-signin2" data-width="370" data-height="40" data-longtitle="true" data-onsuccess="onSignIn" data-onfailure="onSignInFail"></div>');
+				$('head').append('<script src="https://apis.google.com/js/platform.js"><\/script>');
 				$('#bigbutton')[0].nextSibling.style.display='none';
 				document.querySelector('div.p_login_bottom').style.display='none';
 				signInTimeout = setTimeout(onSignInFail, 3000);
